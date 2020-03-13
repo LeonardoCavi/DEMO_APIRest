70983e236046e4dc1378e6b89f6176401ec2e8ad
LeonardoCavi <oliveira.leoc@hotmail.com>
2020-03-13 08:51:30 -0300

    Demo WebAPI Rest

---
+++ Demo_WebAPI/Demo_WebAPI/Controllers/ClientController.cs
@@ -0,0 +1,67 @@
+﻿using Demo_WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo_WebAPI.Controllers
+{
+    public class ClientController : ApiController
+    {
+
+        //public ClientController()
+        //{
+
+        //}
+
+        private static List<Client> Clients = new List<Client>();
+
+        [HttpGet]
+        public List<Client> Get()
+        {
+            return Clients;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post(Client client)
+        {
+            if (string.IsNullOrEmpty(client.Name)) { return BadRequest("Not a valid client name!"); }
+            else if (client.Age < 0 && client.Age > 120) { return BadRequest("Not a valid age name!"); }
+            else if (string.IsNullOrEmpty(client.Profession)) { return BadRequest("Not a valid client profession!"); }
+            else
+            {
+                int? Id = Clients.Count > 0 ? Convert.ToInt32(Clients[Clients.Count - 1].Id): 0;
+
+                if(Id == 0 || Id == null)
+                {
+                    Id = 1;
+                }
+                else
+                {
+                    Id++;
+                }
+
+                Clients.Add(new Client(Id, client.Name, client.Age, client.Profession));
+                return Ok();
+            }           
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var itemToRemove = Clients.SingleOrDefault(r => r.Id == id);
+
+            if(itemToRemove != null)
+            {
+                Clients.Remove(itemToRemove);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest("Not a valid client ID!");
+            }
+        }
+    }
+}
---
+++ Demo_WebAPI/Demo_WebAPI/Models/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_WebAPI.Models
+{
+    public class Client
+    {
+        public int? Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Profession { get; set; }
+
+        public Client(int? id,string name, int age, string profession)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Age = age;
+            this.Profession = profession;
+        }
+    }
+}
